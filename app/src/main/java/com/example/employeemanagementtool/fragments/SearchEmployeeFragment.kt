430fdeec5ba55f79af3b166a318342a8b7495375
Wojciech Sobociński <sobota96@wp.pl>
2020-07-20 19:57:36 +0200

    Reused recyclerView adapter from mainFragment to display records in recyclerview from SearchEmployeeFragment operations/

--- app/src/main/java/com/example/employeemanagementtool/fragments/SearchEmployeeFragment.kt
+++ app/src/main/java/com/example/employeemanagementtool/fragments/SearchEmployeeFragment.kt
@@ -9,13 +9,17 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.employeemanagementtool.R
+import com.example.employeemanagementtool.RecyclerViewAdapter
 import com.example.employeemanagementtool.databinding.SearchEmployeeFragmentBinding
 import com.example.employeemanagementtool.viewmodels.SearchEmployeeViewModel
 
 class SearchEmployeeFragment : Fragment() {
 
    private val searchEmployeeViewModel by viewModels<SearchEmployeeViewModel>()
+    private lateinit var employeeAdapter: RecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +41,16 @@ class SearchEmployeeFragment : Fragment() {
             } else {
                 searchEmployeeViewModel.getEmployeeByFirstAndLastName(firstName, lastName)
             }
+
+            binding.searchRecyclerView.apply {
+                layoutManager = LinearLayoutManager(activity)
+                employeeAdapter = RecyclerViewAdapter()
+                adapter = employeeAdapter
+            }
+
+            searchEmployeeViewModel.searchedResult.observe(viewLifecycleOwner, Observer {
+                employeeAdapter.submitList(it)
+            })
 
         }
         return binding.root
